2c813236aeb9187fdf86be32c3c5efebcfef48f8
Danil Chekantsev <chekantsev.danil2@gmail.com>
2025-06-16 15:21:48 +1000

    First commit

---
+++ FriendZone/FriendZone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FriendZone
+//
+//  Created by Danil Chekantsev on 16/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
